79bb15fb2391b5fab0340d593fdd1e05cb0cab30
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-10-12 19:28:35 +0500

    feat: make mobile nav ui

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const navButton = document.querySelector(".navbar-mobile");
+const navOpenBtn = document.querySelector(".navbar-open");
+const navCloseBtn = document.querySelector(".navbar-close");
+const navMenu = document.querySelector(".navbar-menu");
 const navigation = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const navLogo = document.querySelector(".navbar-logo");
 
-navButton.addEventListener("click", (event) => {
-  if (navigation.classList.contains("hidden")) {
-    navigation.classList.remove("hidden");
+navOpenBtn.style.display = "block";
+navCloseBtn.style.display = "none";
+
+const handleMenuClick = () => {
+  if (navOpenBtn.style.display === "block") {
+    // To open the mobile navigation
+    navOpenBtn.style.display = "none";
+    navCloseBtn.style.display = "block";
+    navigation.classList.remove("nav-hide");
+
+    // To change the bg color of header & navigation
+    navigation.classList.add("nav-active");
+    header.classList.add("nav-active");
+    navLogo.classList.add("nav-active");
   } else {
-    navigation.classList.add("hidden");
+    // To close the mobile navigation
+    navOpenBtn.style.display = "block";
+    navCloseBtn.style.display = "none";
+    navigation.classList.add("nav-hide");
+
+    // To revert the bg color of header & navigation
+    navigation.classList.remove("nav-active");
+    header.classList.remove("nav-active");
+    navLogo.classList.remove("nav-active");
   }
-});
+};
+
+navMenu.addEventListener("click", handleMenuClick);
